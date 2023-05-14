ca415b443776aa20dc51c30b5363a68ba850db32
Sayakb03 <108454817+Sayakb03@users.noreply.github.com>
2023-05-15 00:26:01 +0530

    Update mod4.py

--- mod4.py
+++ mod4.py
@@ -1,8 +1,3 @@
-# Build a circuit using your Raspberry Pi that causes an
-# LED to blink when a push button is NOT pressed.
-# However, the LED should stay on continuously when the push button IS pressed.
-
-
 import RPi.GPIO as GPIO
 import time
 
